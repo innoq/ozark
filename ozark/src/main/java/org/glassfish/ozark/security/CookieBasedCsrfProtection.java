e02c96e014f64d776b80037a95e67ffd41bba1f3
Oliver Tigges <oliver.tigges@innoq.com>
2016-05-10 10:26:22 +0200

    Fixed issue when cookie[] is null.

--- ozark/src/main/java/org/glassfish/ozark/security/CookieBasedCsrfProtection.java
+++ ozark/src/main/java/org/glassfish/ozark/security/CookieBasedCsrfProtection.java
@@ -1,3 +1,42 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * http://glassfish.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
 package org.glassfish.ozark.security;
 
 import javax.annotation.PostConstruct;
@@ -20,6 +59,7 @@ import java.util.UUID;
  * instead of HTTP session. Using this implementation an application can stay completely stateless.
  *
  * See https://www.owasp.org/index.php/Cross-Site_Request_Forgery_%28CSRF%29_Prevention_Cheat_Sheet#General_Recommendation:_Synchronizer_Token_Pattern
+ * @author Oliver Tigges
  */
 @RequestScoped
 @Alternative
@@ -27,6 +67,7 @@ public class CookieBasedCsrfProtection implements Csrf, Serializable {
 
     public static final int MAX_AGE = 3600;
     public static final String COOKIE_NAME = "CsrfToken";
+    private static final String HEADER_SET_COOKIE = "Set-Cookie";
 
     private static final long serialVersionUID = 1141322317114165983L;
 
@@ -56,13 +97,16 @@ public class CookieBasedCsrfProtection implements Csrf, Serializable {
     }
 
     private Optional<Cookie> findCookie() {
+        if (request.getCookies() == null) {
+            return Optional.empty();
+        }
         return Arrays.stream(request.getCookies()).filter(
-                c -> c.getName().equals(COOKIE_NAME)).findFirst();
+                c -> COOKIE_NAME.equals(c.getName())).findFirst();
     }
 
     private void setCookie() {
         final NewCookie cookie = new NewCookie(COOKIE_NAME, token, null, null, null, MAX_AGE, request.isSecure());
-        response.addHeader("Set-Cookie", cookie.toString());
+        response.addHeader(HEADER_SET_COOKIE, cookie.toString());
     }
 
 }
